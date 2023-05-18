4ad40ae0ac0b5bf3d74aaf6dd1d57269d651d26c
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-18 17:25:11 +0200

    versione con prompt

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 const { createApp } = Vue;
 
-// const nome = prompt("Inserisci il tuo nome");
-// const cognome = prompt("Inserisci il tuo cognome");
-// const eta = prompt("Inserisci la tua eta");
-// const via = prompt("Inserisci la via dove abiti");
+function insertData() {
+  const nome = prompt("Inserisci il tuo nome");
+  const cognome = prompt("Inserisci il tuo cognome");
+  const eta = prompt("Inserisci la tua età");
+  const via = prompt("Inserisci la via dove abiti");
 
-createApp({
-    data(){
-        return{
-            nome:`mario`,
-            cognome:`rossi`,
-            eta:`60`,
-            via:`XX Settembre`,
-        }
-    }
+  createApp({
+    data() {
+      return {
+        nome: nome,
+        cognome: cognome,
+        eta: eta,
+        via: via,
+      };
+    },
+  }).mount("#mytext");
+}
 
-}).mount(`#mytext`)
+insertData();
